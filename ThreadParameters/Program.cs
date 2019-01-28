2132cef66e2cb08ea0222d5860a41d54cbf3a412
AlexandrTolstov <alextav_85@mail.ru>
2019-01-28 20:42:24 +0600

    1

--- ThreadParameters/Program.cs
+++ ThreadParameters/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadParameters
@@ -10,6 +11,33 @@ namespace ThreadParameters
     {
         static void Main(string[] args)
         {
+            int x = 0;
+            Console.Write("Введите x = ");
+            x = Int32.Parse(Console.ReadLine());
+
+            MultTable mult = new MultTable(x);
+
+            Thread myThread = new Thread(new ThreadStart(mult.Multiply));
+            myThread.Start();
+
+            Console.ReadLine();
+        }
+    }
+    class MultTable
+    {
+        private int x;
+
+        public MultTable(int _x)
+        {
+            this.x = _x;
+        }
+        public void Multiply()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i*x + "\t");
+                Thread.Sleep(300);
+            }
         }
     }
 }
